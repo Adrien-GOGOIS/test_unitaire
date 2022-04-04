14e175f63113ea07ff0e0f11edf7902a7a74cc37
Adrien GOGOIS <ad.gogois@gmail.com>
2022-04-04 13:48:24 +0200

    Add more tests with string

--- test.js
+++ test.js
@@ -11,3 +11,21 @@ test("is Odd", () => {
 
   expect(result).toBe(false);
 });
+
+test("if is string, return false", () => {
+  const result = isEven("string");
+
+  expect(result).toBe(false);
+});
+
+test("if is string of even number", () => {
+  const result = isEven("12");
+
+  expect(result).toBe(true);
+});
+
+test("if is string of odd number", () => {
+  const result = isEven("13");
+
+  expect(result).toBe(false);
+});
